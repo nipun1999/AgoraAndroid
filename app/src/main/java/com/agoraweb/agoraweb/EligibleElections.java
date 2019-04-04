dd8b95872c407bad237dc1e8b5b81f1830f5d00c
agarw <agarwalnipun12@gmail.com>
2019-04-04 16:07:58 +0530

    few more screens added

---
+++ app/src/main/java/com/agoraweb/agoraweb/EligibleElections.java
@@ -0,0 +1,13 @@
+package com.agoraweb.agoraweb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EligibleElections extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eligible_elections);
+    }
+}
